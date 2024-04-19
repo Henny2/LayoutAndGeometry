f7dd3c6b6a6d7213c6a8eb7ee874fb5fc100c1b6
Henny2 <henrieke.baunack@web.de>
2024-04-18 18:39:12 -1000

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Henrieke Baunack on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
