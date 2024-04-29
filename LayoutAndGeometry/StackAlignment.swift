bb714e2f2440ded98855bb5f35387e4552f547e7
Henny2 <henrieke.baunack@web.de>
2024-04-28 15:05:35 -1000

    adding example for alignment guide modifier for individual views

--- LayoutAndGeometry/StackAlignment.swift
+++ LayoutAndGeometry/StackAlignment.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct StackAlignment: View {
     var body: some View {
+        Spacer()
         // all the views have different sizes so they don't aling on the actual text
         HStack(alignment: .bottom){
             Text("small")
@@ -19,7 +20,7 @@ struct StackAlignment: View {
             Text("extraLarge")
                 .font(.largeTitle)
         }
-        // firstTextBaseline uses the baseline of the text for aligning 
+        // firstTextBaseline uses the baseline of the text for aligning
         HStack(alignment: .firstTextBaseline){
             Text("small")
                 .font(.caption)
@@ -29,6 +30,25 @@ struct StackAlignment: View {
             Text("extraLarge")
                 .font(.largeTitle)
         }
+        Spacer()
+        VStack(alignment: .leading){
+            Text("first line")
+                .alignmentGuide(.leading) { dimension in
+                    print(dimension)
+                    return dimension[.trailing]
+                }
+            Text("second line")
+        }
+        Spacer()
+        VStack(alignment: .leading){
+            ForEach(0..<10){ num in
+                Text("Position \(num)")
+                    .alignmentGuide(.leading) { _ in
+                        return Double(num) * 10
+                    }
+            }
+        }
+        Spacer()
         
     }
 }
