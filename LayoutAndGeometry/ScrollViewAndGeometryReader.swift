3630ac54a62bb5160540c02ac9af63154b704a4d
Henny2 <henrieke.baunack@web.de>
2024-05-19 14:23:58 -1000

    create a spinning helix with geometry reader and a 3d effect

---
+++ LayoutAndGeometry/ScrollViewAndGeometryReader.swift
@@ -0,0 +1,33 @@
+//
+//  ScrollViewAndGeometryReader.swift
+//  LayoutAndGeometry
+//
+//  Created by Henrieke Baunack on 5/19/24.
+//
+
+import SwiftUI
+
+struct ScrollViewAndGeometryReader: View {
+    let colors: [Color] = [.red, .pink, .green, .blue, .green, .purple, .yellow]
+    var body: some View {
+        ScrollView(.vertical){
+                ForEach(0..<50){ index in
+                    GeometryReader{ proxy in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+//                            .frame(width: 200, height:20)
+                            .background(colors[index%7])
+                            .rotation3DEffect(
+                                .degrees(proxy.frame(in: .global).minY/5),
+                                axis: /*@START_MENU_TOKEN@*/(x: 0.0, y: 1.0, z: 0.0)/*@END_MENU_TOKEN@*/
+                            )
+                    }.frame(height: 40)
+            }
+        }
+    }
+}
+
+#Preview {
+    ScrollViewAndGeometryReader()
+}
